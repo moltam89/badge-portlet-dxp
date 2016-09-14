df88970483fb6322e2f1bda505b182677ec73fe3
Peter Borkuti <peter.borkuti@liferay.com>
2016-09-14 09:29:44 +0200

    copy file from badge portlet 6.2 plugin sdk

--- docroot/WEB-INF/src/com/liferay/hu/badge/service/service/impl/BadgeServiceImpl.java
+++ docroot/WEB-INF/src/com/liferay/hu/badge/service/service/impl/BadgeServiceImpl.java
@@ -17,6 +17,17 @@ package com.liferay.hu.badge.service.service.impl;
 import aQute.bnd.annotation.ProviderType;
 
 import com.liferay.hu.badge.service.service.base.BadgeServiceBaseImpl;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import com.liferay.hu.badge.service.model.Badge;
+import com.liferay.hu.badge.service.service.base.BadgeServiceBaseImpl;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.util.GroupThreadLocal;
+import com.liferay.portal.model.User;
+import com.liferay.portal.security.auth.CompanyThreadLocal;
 
 /**
  * The implementation of the badge remote service.
@@ -39,4 +50,94 @@ public class BadgeServiceImpl extends BadgeServiceBaseImpl {
 	 *
 	 * Never reference this class directly. Always use {@link com.liferay.hu.badge.service.service.BadgeServiceUtil} to access the badge remote service.
 	 */
+	 	public boolean addBadge(Date date, long fromUserId, long toUserId,
+			int badgeType, String description) {
+
+		long badgeId = 0;
+		try {
+			badgeId = counterLocalService.increment();
+		} catch (SystemException e) {
+			e.printStackTrace();
+			return false;
+		}
+
+		long companyId = CompanyThreadLocal.getCompanyId();
+		long groupId = GroupThreadLocal.getGroupId();
+
+		long currentUserId = 0;
+		User currentUser;
+		try {
+			currentUser = userService.getCurrentUser();
+			if (currentUser != null) {
+				currentUserId = currentUser.getUserId();
+			}
+		} catch (PortalException e1) {
+			e1.printStackTrace();
+		} catch (SystemException e1) {
+			e1.printStackTrace();
+		}
+
+		
+		String fromUserName = _getUserFullNameById(fromUserId);
+		String toUserName = _getUserFullNameById(toUserId);
+
+		Badge badge = badgePersistence.create(badgeId);
+
+		badge.setBadgeType(badgeType);
+		badge.setDescription(description);
+		badge.setToUser(toUserId);
+		badge.setFromUser(fromUserId);
+		badge.setAssignDate(date);
+		badge.setToUserFullName(toUserName);
+		badge.setFromUserFullName(fromUserName);
+
+		badge.setCompanyId(companyId);
+		badge.setCreateDate(new Date());
+		badge.setGroupId(groupId);
+		badge.setUserId(currentUserId);
+
+		try {
+			badgePersistence.update(badge);
+		} catch (SystemException e) {
+			e.printStackTrace();
+			return false;
+		}
+
+		return true;
+	}
+
+	private String _getUserFullNameById(long userId) {
+		String userName = "";
+		try {
+			User user = userLocalService.fetchUser(userId);
+			userName = user.getFullName();
+		} catch (SystemException e) {
+			e.printStackTrace();
+		}
+
+		return userName;
+	}
+
+	public List<Badge> getBadges() {
+		List<Badge> badges = new ArrayList<Badge>();
+
+		try {
+			badges = badgePersistence.findAll();
+		} catch (SystemException e) {
+			e.printStackTrace();
+		}
+		return badges;
+	}
+
+	public List<Badge> getBadges(int badgeType) {
+		List<Badge> badges = new ArrayList<Badge>();
+
+		try {
+			badges = badgePersistence.findBybadgeType(badgeType);
+		} catch (SystemException e) {
+			e.printStackTrace();
+		}
+		return badges;
+	}
+
 }
